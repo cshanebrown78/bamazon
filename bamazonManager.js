68677702a819d1f99993023b18f5afc9b98b5339
cshanebrown78 <cshanebrown78@gmail.com>
2020-04-14 13:10:53 -0500

    coded managers choice function

--- bamazonManager.js
+++ bamazonManager.js
@@ -21,5 +21,44 @@ var connection = mysql.createConnection({
   connection.connect(function(err) {
     if (err) throw err;
     // console.log("Connected")
-    displayItems();
+    managerChoice();
   });
+
+
+  function managerChoice() {
+    console.log("\n")
+  inquirer
+  .prompt([
+    {
+      name: "choice",
+      type: "list",
+      message: "What is the Item ID of the product you wish to purchase?",
+      choices: [
+          "View Products for Sale",
+          "View Low Inventory",
+          "Add to Inventory",
+          "Add New Product"
+      ]
+    },
+  ])
+  .then(function(answer) {
+    
+    switch (choice) {
+        case "View Products for Sale":
+            displayItems();
+            break;
+
+        case "View Low Inventory":
+            lowInventory();
+            break;
+        
+        case "Add to Inventory":
+            addInventory();
+            break;
+
+        case "Add New Product":
+            addProduct();
+            break;
+    }
+  });
+ }
